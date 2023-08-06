467627007f60b1ebc8e0072ea511106b49684f98
Tyluur <contact@tyluur.com>
2023-08-06 13:12:52 -0400

    fix: correct file class loader

--- src/main/java/novite/rs/utility/tools/FileClassLoader.java
+++ src/main/java/novite/rs/utility/tools/FileClassLoader.java
@@ -1,7 +1,11 @@
 package novite.rs.utility.tools;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 
 /**
@@ -13,21 +17,60 @@ public class FileClassLoader {
 	/**
 	 * Gets all of the classes in a directory
 	 *
-	 * @param directory
-	 *            The directory to iterate through
+	 * @param packageName The packageName to iterate through
 	 * @return The list of classes
 	 */
-	public static List<Object> getClassesInDirectory(String directory) {
-		List<Object> classes = new ArrayList<Object>();
-		for (File file : new File("./bin/" + directory.replace(".", "/")).listFiles()) {
+	public static List<Object> getClassesInDirectory(String packageName) {
+		try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			assert classLoader != null;
+			String path = packageName.replace('.', '/');
+			Enumeration<URL> resources = classLoader.getResources(path);
+			List<File> dirs = new ArrayList<>();
+			while (resources.hasMoreElements()) {
+				URL resource = resources.nextElement();
+				dirs.add(new File(resource.getFile().replaceAll("%20", " ")));
+			}
+			List<Class> classes = new ArrayList<>();
+			for (File directory : dirs) {
+				classes.addAll(findClasses(directory, packageName));
+			}
+			List<Object> list = new ArrayList<>();
+			for (Class clazz : classes) {
+				if (clazz.isAnnotation()) {
+					continue;
+				}
+				list.add(clazz.newInstance());
+			}
+			return list;
+		} catch (IllegalAccessException | InstantiationException | IOException e) {
+			e.printStackTrace();
+			return Collections.EMPTY_LIST;
+		}
+	}
+
+	private static List<Class> findClasses(File directory, String packageName) {
+		List<Class> classes = new ArrayList<>();
+		if (!directory.exists()) {
+			return classes;
+		}
+		File[] files = directory.listFiles();
+		if (files == null) {
+			return classes;
+		}
+		for (File file : files) {
 			if (file.getName().contains("$")) {
 				continue;
 			}
-			try {
-				Object objectEvent = (Class.forName(directory + "." + file.getName().replace(".class", "")).newInstance());
-				classes.add(objectEvent);
-			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-				e.printStackTrace();
+			if (file.isDirectory()) {
+				assert !file.getName().contains(".");
+				classes.addAll(findClasses(file, packageName + "." + file.getName()));
+			} else if (file.getName().endsWith(".class")) {
+				try {
+					classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		return classes;
